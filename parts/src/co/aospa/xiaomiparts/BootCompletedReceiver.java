b0655478224a6748128ae386fa3b11d549d9f1ff
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-15 22:07:10 +0530

    Revert "marble: Add support for high touch polling rate"
    On marble this caused ghost touches when this was enabled.
    With the OSS kernel, this has been removed from gt9916r driver.
    Few other devices seem to support it via Touch_Report_Rate touchfeature
    mode, and can be added back after further testing.
    
    This reverts commit a07bdcbadfc31da40fc06d8d89e3ede4ebdc63b1.
    
    Change-Id: If5928d0e059e814702fb67522ec1a4b99a94da11

--- parts/src/co/aospa/xiaomiparts/BootCompletedReceiver.java
+++ parts/src/co/aospa/xiaomiparts/BootCompletedReceiver.java
@@ -32,7 +32,6 @@ import co.aospa.xiaomiparts.doze.AodBrightnessService;
 import co.aospa.xiaomiparts.doze.PocketService;
 import co.aospa.xiaomiparts.gestures.GestureUtils;
 import co.aospa.xiaomiparts.thermal.ThermalUtils;
-import co.aospa.xiaomiparts.touch.HighTouchPollingService;
 import co.aospa.xiaomiparts.touch.TouchNonUiService;
 import co.aospa.xiaomiparts.touch.TouchOrientationService;
 
@@ -52,7 +51,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         AodBrightnessService.startService(context);
         PocketService.startService(context);
         NfcCameraService.startService(context);
-        HighTouchPollingService.startService(context);
         TouchOrientationService.startService(context);
         TouchNonUiService.startService(context);
         ThermalUtils.startService(context);
