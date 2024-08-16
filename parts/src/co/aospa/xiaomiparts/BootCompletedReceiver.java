3536e13ec992a02a75a9a4732f5fe504cca64670
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-16 07:52:39 +0530

    marble: parts: Add nonui touch notifier service
    Inspired by https://github.com/cupid-development/android_device_xiaomi_sm8450-common/commit/c9aa23766eb2c410c255d88cdce0c7f6179d2fd4
    
    Change-Id: I7a8c276f894403b7576aa03eacdd5aabcc095aa9

--- parts/src/co/aospa/xiaomiparts/BootCompletedReceiver.java
+++ parts/src/co/aospa/xiaomiparts/BootCompletedReceiver.java
@@ -33,6 +33,7 @@ import co.aospa.xiaomiparts.doze.PocketService;
 import co.aospa.xiaomiparts.gestures.GestureUtils;
 import co.aospa.xiaomiparts.thermal.ThermalUtils;
 import co.aospa.xiaomiparts.touch.HighTouchPollingService;
+import co.aospa.xiaomiparts.touch.TouchNonUiService;
 import co.aospa.xiaomiparts.touch.TouchOrientationService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -53,6 +54,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         NfcCameraService.startService(context);
         HighTouchPollingService.startService(context);
         TouchOrientationService.startService(context);
+        TouchNonUiService.startService(context);
         ThermalUtils.startService(context);
         GestureUtils.onBootCompleted(context);
         overrideHdrTypes(context);
