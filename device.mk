da4b247c0632b2c68e53a014af83434f6f78e658
FlowerSea0208 <zzp060208@gmail.com>
2024-09-29 00:58:45 +0800

    marble: Switch to source-built fx.tunnel interface
    Change-Id: I1b96a1df3758106b7516ee7b9ac90bd9374f6d1c

--- device.mk
+++ device.mk
@@ -190,7 +190,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint@2.1.vendor \
+    vendor.xiaomi.hardware.fx.tunnel@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
