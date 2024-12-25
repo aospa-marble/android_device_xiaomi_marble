a65fc82668861019c1f9f7d8b9ef4b35143a99dc
orges <orges@orgesified.com>
2024-12-25 14:53:43 +0100

    Revert "marble: Build mtdservice interface lib from source"
    This reverts commit 86d8b146e142de2cbb07c2ae9086a7a2d42b5ba7.

--- device.mk
+++ device.mk
@@ -312,8 +312,7 @@ PRODUCT_COPY_FILES += \
 # Mlipay
 PRODUCT_PACKAGES += \
     IFAAService \
-    vendor.xiaomi.hardware.mlipay@1.1.vendor \
-    vendor.xiaomi.hardware.mtdservice@1.0.vendor
+    vendor.xiaomi.hardware.mlipay@1.1.vendor
 
 # NDK
 NEED_AIDL_NDK_PLATFORM_BACKEND := true
