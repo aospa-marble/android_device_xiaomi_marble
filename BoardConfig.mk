f6681da117274c2661dfa920428f5fbf4540c385
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-04-18 21:35:06 +0530

    marble: Implement torch strength control
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    CO-authored-by: Cyber Knight <cyberknight755@gmail.com>
    Change-Id: I150578fd4d234ffd2a29788ab0b5560f578d330e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,9 @@ BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES  := true
 BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 
+# Camera
+TARGET_CAMERA_SERVICE_EXT_LIB := //$(DEVICE_PATH):libcameraservice_extension.xiaomi_taro
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(HARDWARE_PATH)/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
