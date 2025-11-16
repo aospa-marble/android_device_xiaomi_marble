0ea7009dbeed4ce0a5274c49b4b24587e329e500
ShadeDark6 <nizamfa6@gmail.com>
2025-11-16 14:50:51 +0000

    marble: Address missing SetTaskProfiles symbol in camera libs This is because DECLS was removed in [1] on Android 15 QPR2, and caused SetTaskProfiles() to be hidden, so shim SetTaskProfiles with libprocessgroup_shim.
    [1]: https://cs.android.com/android/_/android/platform/system/core/+/3e7c17a8e7ad7275ea3a65642608b1c0becb38ad
    
    orges: add fixup only option
    
    Signed-off-by: orges <orges@orgesified.com>

--- extract-files.sh
+++ extract-files.sh
@@ -28,6 +28,7 @@ CLEAN_VENDOR=true
 
 KANG=
 SECTION=
+FIXUP_ONLY=false
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
@@ -41,6 +42,9 @@ while [ "${#}" -gt 0 ]; do
              SECTION="${2}"
              shift
              CLEAN_VENDOR=false
+             ;;
+        -f | --fixup-only)
+             FIXUP_ONLY=true
              ;;
          *)
              SRC="${1}"
@@ -57,27 +61,36 @@ function blob_fixup() {
     case "${1}" in
         vendor/bin/hw/android.hardware.security.keymint-service-qti | vendor/lib64/libqtikeymint.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --add-needed "android.hardware.security.rkp-V1-ndk_platform.so" "${2}"
+            "${PATCHELF}" --print-needed "${2}" | grep -q "android.hardware.security.rkp-V1-ndk_platform.so" || \
+                "${PATCHELF}" --add-needed "android.hardware.security.rkp-V1-ndk_platform.so" "${2}"
             ;;
         vendor/bin/hw/dolbycodec2 | vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service | vendor/bin/hw/vendor.dolby.media.c2@1.0-service)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            "${PATCHELF}" --print-needed "${2}" | grep -q "libstagefright_foundation-v33.so" || \
+                "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
         vendor/bin/hw/mfp-daemon | vendor/lib64/hw/displayfeature.default.so | vendor/lib64/hw/audio.primary.taro.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            "${PATCHELF}" --print-needed "${2}" | grep -q "libstagefright_foundation-v33.so" || \
+                "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
         vendor/bin/hw/vendor.qti.hardware.display.composer-service)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --remove-needed "libutils.so" "${2}"
-            "${PATCHELF}" --add-needed "libutils-v32.so" "${2}"
-            "${PATCHELF}" --add-needed "libutils-shim.so" "${2}"
+            "${PATCHELF}" --print-needed "${2}" | grep -q "libutils.so" && \
+                "${PATCHELF}" --remove-needed "libutils.so" "${2}"
+            "${PATCHELF}" --print-needed "${2}" | grep -q "libutils-v32.so" || \
+                "${PATCHELF}" --add-needed "libutils-v32.so" "${2}"
+            "${PATCHELF}" --print-needed "${2}" | grep -q "libutils-shim.so" || \
+                "${PATCHELF}" --add-needed "libutils-shim.so" "${2}"
             ;;
         vendor/bin/hw/vendor.qti.secure_element@1.2-service)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "jcos_nq_client-v1.so" "jcos_nq_client.so" "${2}"
-            "${PATCHELF}" --replace-needed "ls_nq_client-v1.so" "ls_nq_client.so" "${2}"
-            "${PATCHELF}" --replace-needed "se_nq_extn_client-v1.so" "se_nq_extn_client.so" "${2}"
+            "${PATCHELF}" --print-needed "${2}" | grep -q "jcos_nq_client-v1.so" && \
+                "${PATCHELF}" --replace-needed "jcos_nq_client-v1.so" "jcos_nq_client.so" "${2}"
+            "${PATCHELF}" --print-needed "${2}" | grep -q "ls_nq_client-v1.so" && \
+                "${PATCHELF}" --replace-needed "ls_nq_client-v1.so" "ls_nq_client.so" "${2}"
+            "${PATCHELF}" --print-needed "${2}" | grep -q "se_nq_extn_client-v1.so" && \
+                "${PATCHELF}" --replace-needed "se_nq_extn_client-v1.so" "se_nq_extn_client.so" "${2}"
             ;;
         vendor/etc/camera/marble*_motiontuning.xml)
             [ "$2" = "" ] && return 0
@@ -93,15 +106,23 @@ function blob_fixup() {
             ;;
         vendor/lib64/c2.dolby.client.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --add-needed "libcodec2_hidl_shim.so" "${2}"
+            "${PATCHELF}" --print-needed "${2}" | grep -q "libcodec2_hidl_shim.so" || \
+                "${PATCHELF}" --add-needed "libcodec2_hidl_shim.so" "${2}"
             ;;
         vendor/lib64/libwvhidl.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            "${PATCHELF}" --print-needed "${2}" | grep -q "libcrypto_shim.so" || \
+                "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
         vendor/lib64/vendor.libdpmframework.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --add-needed "libhidlbase_shim.so" "${2}"
+            "${PATCHELF}" --print-needed "${2}" | grep -q "libhidlbase_shim.so" || \
+                "${PATCHELF}" --add-needed "libhidlbase_shim.so" "${2}"
+            ;;
+        vendor/lib64/libcamxcommonutils.so | vendor/lib64/libmialgoengine.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --print-needed "${2}" | grep -q "libprocessgroup_shim.so" || \
+                "${PATCHELF}" --add-needed "libprocessgroup_shim.so" "${2}"
             ;;
         *)
              return 1
@@ -114,6 +135,29 @@ function blob_fixup() {
 function blob_fixup_dry() {
      blob_fixup "$1" ""
 }
+
+if [ "${FIXUP_ONLY}" = true ]; then
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false false
+    parse_file_list "${MY_DIR}/proprietary-files.txt" "${SECTION}"
+    DEST_LIST=("${PRODUCT_COPY_FILES_DEST[@]}" "${PRODUCT_PACKAGES_DEST[@]}")
+    OUTPUT_ROOT="${ANDROID_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary"
+    set +e
+    for BLOB in "${DEST_LIST[@]}"; do
+        BLOB_FILE="${OUTPUT_ROOT}/${BLOB}"
+        if [ ! -f "${BLOB_FILE}" ]; then
+            continue
+        fi
+        if blob_fixup_dry "${BLOB}"; then
+            PRE_HASH=$(md5sum "${BLOB_FILE}" | cut -d' ' -f1)
+            blob_fixup "${BLOB}" "${BLOB_FILE}"
+            POST_HASH=$(md5sum "${BLOB_FILE}" | cut -d' ' -f1)
+            [ "${PRE_HASH}" != "${POST_HASH}" ] && printf '  + Fixed up %s\n' "${BLOB}"
+        fi
+    done
+    set -e
+    "${MY_DIR}/setup-makefiles.sh"
+    exit 0
+fi
 
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
